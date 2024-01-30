582cc8022d39bd59ef62399e0943de335bb54806
dkiriinya <dkiriinya06sc@gmail.com>
2024-01-31 00:09:58 +0300

    learnt how to use props

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Links from "./Links"; // Make sure the import is correct and uses the correct component name
 
-function About() {
+function About(props) {
+  const hasBio = props.bio;
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      {hasBio && <p>{props.bio}</p>}
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+      {/* Render the Links component */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Links(props) {
+    console.log(props.github)
+    console.log(props.linkedin)
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={props.github} target="_blank" rel="noopener noreferrer">
+        {props.github}
+      </a>
+      <a href={props.linkedin} target="_blank" rel="noopener noreferrer">
+      {props.linkedin}
+        
+      </a>
+    </div>
+  );
+}
+
+export default Links;
